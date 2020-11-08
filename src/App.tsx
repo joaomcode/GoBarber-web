2c1dc9e8e6b8442bd5464c73bed43435d841dad6
joaomcode <joaomarcelo.co@hotmail.com>
2020-11-08 10:57:22 -0300

    Estrutura da página - 05 - Página de cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import GlobalStyle from './styles/global';
-import SignIn from './pages/SingIn';
+// import SignIn from './pages/SingIn';
+import SignUp from './pages/SingUp';
 
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <SignUp />
     <GlobalStyle />
   </>
 );
