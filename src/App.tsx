1339ef9e490b4fc79f3b48c97c3bba37e0cfb805
joaomcode <joaomarcelo.co@hotmail.com>
2020-11-22 15:45:25 -0300

    Estrutura da página - 11 - Validação de login

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
 import GlobalStyle from './styles/global';
-// import SignIn from './pages/SingIn';
-import SignUp from './pages/SingUp';
+import SignIn from './pages/SingIn';
+// import SignUp from './pages/SingUp';
 
 const App: React.FC = () => (
   <>
-    <SignUp />
+    <SignIn />
     <GlobalStyle />
   </>
 );
