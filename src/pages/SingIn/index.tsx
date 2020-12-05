9ca0ea83f8fc8a522b97edc1a4a53cf61659a0c9
joaomcode <joaomarcelo.co@hotmail.com>
2020-12-05 15:40:54 -0300

    Autenticação - 04 - Mantendo usuário no storage

--- src/pages/SingIn/index.tsx
+++ src/pages/SingIn/index.tsx
@@ -22,7 +22,9 @@ interface SingInFormData {
 const SingIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
-  const { signIn } = useContext(AuthContext);
+  const { user, signIn } = useContext(AuthContext);
+
+  console.log(user);
 
   const handleSubmit = useCallback(
     async (data: SingInFormData) => {
